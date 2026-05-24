92501735c2d6652ab9c77307ded6fa8f49bd0bba
ronalORT <thegaesposo@gmail.com>
2026-05-24 17:37:20 -0400

    act version a 2.3

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // actualización del service worker en clientes existentes.
 // ============================================================
 
-const CACHE_NAME = "taller-costura-v2.2";
+const CACHE_NAME = "taller-costura-v2.3";
 
 // ============================================================
 // ASSETS LOCALES - Pre-cacheados durante el evento install
